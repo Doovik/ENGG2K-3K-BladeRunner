f150afdfadf2cd87abdcb02af949d574c2f1429a
Hugo-28 <hcawsey@spin.net.au>
2024-08-27 14:51:13 +1000

    got 2 sensors working

--- example_esp32_ultrasonic/example_esp32_ultrasonic.ino
+++ example_esp32_ultrasonic/example_esp32_ultrasonic.ino
@@ -1,26 +1,41 @@
-from machine import Pin, time_pulse_us
-import time
+#include <NewPing.h>
 
-# Define the trigger and echo pins
-trigger_pin = Pin(5, Pin.OUT)
-echo_pin = Pin(4, Pin.IN)
+#define S1_TRIGGER_PIN 25
+#define S1_ECHO_PIN 26
+#define S2_TRIGGER_PIN 27
+#define S2_ECHO_PIN 14
 
-# Define a function to measure the distance
-def measure_distance():
-    # Trigger the sensor by sending a 10 microsecond pulse
-    trigger_pin.on()
-    time.sleep_us(10)
-    trigger_pin.off()
-    
-    # Measure the duration of the echo pulse
-    duration = time_pulse_us(echo_pin, 1, 30000)
-    
-    # Convert the duration to distance in centimeters
-    distance = duration / 58
-    
-    return distance
+#define maxDist 450
 
-while True:
-    distance = measure_distance()
-    print(int(distance))
-    time.sleep(.3)
+NewPing S1_sonar(S1_TRIGGER_PIN, S1_ECHO_PIN, maxDist);
+NewPing S2_sonar(S2_TRIGGER_PIN, S2_ECHO_PIN, maxDist);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int S1_distance = S1_sonar.ping_cm();
+  delay(10);  // Wait for the first echo to dissipate
+  int S2_distance = S2_sonar.ping_cm();
+
+  if(S1_distance < 1) {
+    Serial.println("S1 Unable to read");
+  } else {
+    Serial.print("S1: ");
+    Serial.print(S1_distance);
+    Serial.println("cm");
+  }
+
+  if(S2_distance < 1) {
+    Serial.println("S2 Unable to read");
+  } else {
+    Serial.print("S2: ");
+    Serial.print(S2_distance);
+    Serial.println("cm");
+  }
+
+  delay(10);  // Wait before starting the next loop
+}
