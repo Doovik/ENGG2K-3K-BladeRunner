4b3b850c0d7509f37073b3958b786a7b7a93efe1
Misha Asprey <misha.asprey@gmail.com>
2024-09-10 15:49:21 +1000

    JSON read & write improvements

--- CCP.java
+++ CCP.java
@@ -8,9 +8,11 @@ import org.json.simple.parser.*;
 //import java.util.Iterator;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CCP {
+    //enum for current carriage status
     enum Status {
         STOPPED,
         STARTED,
@@ -20,36 +22,19 @@ public class CCP {
         CRASH,
         STOPPED_AT_STATION
     }
+    //initialise empty values for jsonRead
+    static String client_type;
+    static String message;
+    static String client_id;
+    static String timestamp;
+    static String action;
     static String fileLoc = "";
+    //initialise JSONParser
+    static JSONParser parser = new JSONParser();
+    static JSONObject jsonWrite = new JSONObject();
+
     final static String client = "ccp";
     public static void main(String[] args) {
-        //JSON read
-        //initialise empty values;
-        String client_type;
-        String message;
-        String client_id;
-        String timestamp;
-        String action;
-        //initialise JSONParser
-        JSONParser parser = new JSONParser();
-        try {
-            JSONObject obj = (JSONObject)parser.parse(new FileReader(fileLoc)); //Get JSONObject
-            //fetching values from JSON Object
-            client_type = (String) obj.get("client_type");
-            message = (String) obj.get("message");
-            client_id = (String) obj.get("client_id");
-            timestamp = (String) obj.get("timestamp");
-            action = (String) obj.get("action");
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-        catch(ParseException e){
-            e.printStackTrace();
-        }
         //UDP
         int port = 3014;
         try (DatagramSocket socket = new DatagramSocket(port)) {
@@ -57,6 +42,8 @@ public class CCP {
 
             System.out.println("Server is listening on port " + port);
 
+            jsonRead(); //TODO put jsonRead and jsonWrite inside the loop
+            jsonWrite(jsonWrite, "TEST-MSG", "TEST-ID", Status.STOPPED_AT_STATION, "TEST-SID");
             while (true) {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
@@ -70,15 +57,16 @@ public class CCP {
                 int clientPort = packet.getPort();
                 packet = new DatagramPacket(response.getBytes(), response.length(), address, clientPort);
                 socket.send(packet);
+                //Write to JSON
+                
             }
         } catch (Exception ex) {
             System.out.println("Server exception: " + ex.getMessage());
             ex.printStackTrace();
         }
 
-        //JSON write
-        JSONObject jsonWrite = new JSONObject();
-        jsonWrite(jsonWrite, " ", " ", Status.STOPPED, " "); //TODO - determine values
+        //JSON read/write
+        //jsonWrite(jsonWrite, " ", " ", Status.STOPPED, " "); //TODO - determine values
 
 
     }
@@ -89,9 +77,40 @@ public class CCP {
         jobj.put("message", message);
         jobj.put("client_id", clientID);
         jobj.put("timestamp", " "); //TODO - check which func can do the format
-        jobj.put("status", status);
+        jobj.put("status", status.toString());
         if (status == Status.STOPPED_AT_STATION) {
             jobj.put("station_id", stationID);
+        }
+        try (FileWriter file = new FileWriter("C:\\ENGG3000_BR_2\\New folder\\ENGG2K-3K-BladeRunner\\test.json")) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(jobj.toJSONString()); 
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void jsonRead() {
+        try {
+            JSONObject obj = (JSONObject)parser.parse(new FileReader("C:\\ENGG3000_BR_2\\New folder\\ENGG2K-3K-BladeRunner\\test.json")); //Get JSONObject
+            //fetching values from JSON Object
+            client_type = (String) obj.get("client_type");
+            message = (String) obj.get("message");
+            client_id = (String) obj.get("client_id");
+            timestamp = (String) obj.get("timestamp");
+            action = (String) obj.get("action");
+            System.out.println(client_type + "\n" + message + "\n" + client_id + "\n" + 
+                               timestamp + "\n" + action + "\n" + action); //TODO - remove or replace with logger
+        }
+        catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
         }
     }
 }
